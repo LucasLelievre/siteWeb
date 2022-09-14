610e86943015f737f65ee2561cbb8f293a0f16ab
LucasLelievre <lucas.lelievre98@gmail.com>
2022-09-14 22:41:46 +0200

    html semantics

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -6,7 +6,7 @@ document.querySelector("#searchbar").addEventListener('keypress', (event) => {
     if (event.keyCode == 13) {
         let val = document.querySelector("#searchbar").value;
         if (val != "") {
-            if (/^(https?:\/\/)?([\w]+\.)(\.?[\w]{2,})+(\/\S+)?$/.test(val)) {
+            if (/^(https?:\/\/)?([\w-]+\.)(\.?[\w]{2,})+(\/\S+)?$/.test(val)) {
                 location.href = /^https?:\/\//.test(val) ? val : "https://"+val;
             } else {
                 location.href = "https://duckduckgo.com/?q=" + encodeURIComponent(val);
